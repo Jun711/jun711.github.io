0ff8cc940cf6d606cbc30c0f20f3599137f4abab
Jun Goh <cygoh@umich.edu>
2019-08-18 02:23:01 -0700

    [ui] change read progress bar to rainbow colour

--- assets/js/read-progress.js
+++ assets/js/read-progress.js
@@ -2,12 +2,11 @@ document.addEventListener('scroll', _ => {
   var docElem = document.documentElement;
   var docBody = document.body;
   var docScrollTop = (docBody.scrollTop || docElem.scrollTop);
-  var docScrollHeight = (docBody.scrollHeight || docElem.clientHeight);
 
-  readPercent = docScrollTop / (docScrollHeight - docElem.clientHeight) * 100;
+  readPercent = docScrollTop / (docElem.scrollHeight - docElem.clientHeight) * 100;
 
   if (readPercent > 0) {
-    progressBar = document.querySelector('.progress');
+    progressBar = document.querySelector('#progress-bar');
     progressBar.style.setProperty('--scroll', readPercent + '%');
   } else {
     progressBar.style.setProperty('--scroll', '0%');
